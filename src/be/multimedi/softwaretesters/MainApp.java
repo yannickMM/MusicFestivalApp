8b80d6deda2cc325530c24f9fc05c69ab75ee8f0
Yannick <vanham.yannick@gmail.com>
2020-11-25 17:14:36 +0100

    Create package structure and main class

---
+++ src/be/multimedi/softwaretesters/MainApp.java
@@ -0,0 +1,11 @@
+package be.multimedi.softwaretesters;
+
+/**
+ * @author Yannick Van Ham
+ * created on Wednesday, 25/11/2020
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        
+    }
+}
